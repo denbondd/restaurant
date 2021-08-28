e79a270c22daa8f6ad1e2020d7608a855a61cbb7
Denys Bondarenko <denbon04@gmail.com>
2021-08-28 13:37:03 +0300

    initial commit

---
+++ src/main/java/com/denbondd/restaurant/db/ConnectionPool.java
@@ -0,0 +1,34 @@
+package com.denbondd.restaurant.db;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionPool {
+    private static ConnectionPool instance;
+
+    private DataSource ds;
+
+    private ConnectionPool() {
+        try {
+            Context context = new InitialContext();
+            ds = (DataSource) context.lookup("jdbc/restaurant");
+        } catch (NamingException ex) {
+            throw new IllegalStateException("Cannot init DBManager", ex);
+        }
+    }
+
+    public static ConnectionPool getInstance() {
+        if (instance == null) {
+            instance = new ConnectionPool();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
+}
